7441e6e4f6ffd95b87609777721eb508889e3cdb
Lawrence SXP <lawrencesxp@Lawrences-MacBook-Pro.local>
2025-01-04 15:01:55 -0500

    yes

--- BinarySearch.py
+++ BinarySearch.py
@@ -18,24 +18,3 @@ while low<=high:
         high = mid - 1
     else: 
         print("man")
-    
-# array = [1, 2, 3, 4, 5, 6, 7]
-# target = 6
-
-# low, high = 0, len(array) - 1
-
-# while low <= high:
-#     mid = (low + high) // 2  # Find the middle index
-#     pos = array[mid]
-
-#     if pos == target:
-#         print(f"Target is found and it is: {pos}")
-#         break
-#     elif pos < target:
-#         print("Target is probably to the right")
-#         low = mid + 1
-#     else:
-#         print("Target is probably to the left")
-#         high = mid - 1
-# else:
-#     print("Target not found.")
